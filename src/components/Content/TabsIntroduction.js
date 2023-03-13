05cb0369e20932fbe436f9736f8c94075b8a89cf
Hanna <hannapleshko2000@mail.ru>
2023-03-13 15:03:13 +0300

    modal

--- src/components/Content/TabsIntroduction.js
+++ src/components/Content/TabsIntroduction.js
@@ -6,28 +6,29 @@ import TabsListUnstyled from '@mui/base/TabsListUnstyled';
 import TabPanelUnstyled from '@mui/base/TabPanelUnstyled';
 import { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
 import TabUnstyled, { tabUnstyledClasses } from '@mui/base/TabUnstyled';
+import ModalTab from '../../components/ModalTab/ModalTab';
 
 const blue = {
-    50: '#f6f7f7',
-    100: '#d2e3eb',
-    200: '#ADC8D9',
-    300: '#90B1C8',
-    400: '#84ACC3',
-    500: '#7c9ab4',
-    600: '#627d94',
+  50: '#f6f7f7',
+  100: '#d2e3eb',
+  200: '#ADC8D9',
+  300: '#90B1C8',
+  400: '#84ACC3',
+  500: '#7c9ab4',
+  600: '#627d94',
 };
 
 const grey = {
-    50: '#f6f8fa',
-    100: '#eaeef2',
-    200: '#d0d7de',
-    300: '#afb8c1',
-    400: '#8c959f',
-    500: '#6e7781',
-    600: '#57606a',
-    700: '#424a53',
-    800: '#32383f',
-    900: '#24292f',
+  50: '#f6f8fa',
+  100: '#eaeef2',
+  200: '#d0d7de',
+  300: '#afb8c1',
+  400: '#8c959f',
+  500: '#6e7781',
+  600: '#57606a',
+  700: '#424a53',
+  800: '#32383f',
+  900: '#24292f',
 };
 
 const Tab = styled(TabUnstyled)`
@@ -72,7 +73,7 @@ const Tab = styled(TabUnstyled)`
 // );
 
 const TabsList = styled(TabsListUnstyled)(
-    ({ theme }) => `
+  ({ theme }) => `
   min-width: 400px;
   background-color: ${blue[400]};
   border-radius: 12px;
@@ -87,20 +88,25 @@ const TabsList = styled(TabsListUnstyled)(
 
 function TabsIntroduction({ content }) {
 
-    return (
-        <div className={style['tabs-content']}>
-            <TabsUnstyled defaultValue={0}>
-                <TabsList>
-                    <Tab>Create new {content}</Tab>
-                    <Tab>Update {content}</Tab>
-                    <Tab>Delete {content}</Tab>
-                </TabsList>
-                {/* <TabPanel value={0}>My account page</TabPanel>
-                <TabPanel value={1}>Profile page</TabPanel>
-                <TabPanel value={2}>Language page</TabPanel> */}
-            </TabsUnstyled>
-        </div>
-    )
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
+  return (
+    <div className={style['tabs-content']}>
+      <TabsUnstyled defaultValue={0}>
+        <TabsList>
+          <Tab onClick={handleOpen}>Create {content}</Tab>
+          <Tab onClick={handleOpen}>Update {content}</Tab>
+          <Tab onClick={handleOpen}>Delete {content}</Tab>
+        </TabsList>
+
+        {open ? <ModalTab open={open} handleClose={handleClose} /> : null}
+
+      </TabsUnstyled>
+    </div>
+  )
 }
 
 export default TabsIntroduction
